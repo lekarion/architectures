6a3178db7365900328a4b72e52f1e626ffcb9f96
lekarion <lekarion.dev@gmail.com>
2023-12-06 15:56:49 +0200

    feat: project moved to xcodegen

--- ArchitecturesTests/ArchitecturesTests.swift
+++ ArchitecturesTests/ArchitecturesTests.swift
@@ -9,7 +9,6 @@ import XCTest
 @testable import Architectures
 
 final class ArchitecturesTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -32,5 +31,4 @@ final class ArchitecturesTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }

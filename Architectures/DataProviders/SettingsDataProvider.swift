627e84bad3d78544525f06c3ffca8777299642a1
lekarion <lekarion.dev@gmail.com>
2023-12-14 20:00:36 +0200

    chore: test for settings provider

--- Architectures/DataProviders/SettingsDataProvider.swift
+++ Architectures/DataProviders/SettingsDataProvider.swift
@@ -15,29 +15,52 @@ class SettingsDataProvider: SettingsProviderInterface {
     var sortingOrder: SortingOrder {
         get {
             var result: SortingOrder = .none
-            safe(sync: true) { _ in
-                result = UserDefaults.sortingOrder
+            safe(sync: true) { [weak self] in
+                guard let readValue = self?.readValue(String.self, for: Keys.sortingOrder) else { return }
+                guard let finalValue = SortingOrder(rawValue: readValue) else { return }
+                result = finalValue
             }
             return result
         }
 
         set {
-            safe { _ in
-                UserDefaults.sortingOrder = newValue
+            safe { [weak self] in
+                self?.writreValue(newValue.rawValue, for: Keys.sortingOrder)
             }
         }
     }
 
-    private func safe(sync: Bool = false, handler: @escaping (UserDefaults.Context) -> Void) {
-        let context = UserDefaults.Context(identifier)
+    private func safe(sync: Bool = false, handler: @escaping () -> Void) {
         let queue = DispatchQueue.main
 
         if sync {
-            queue.sync { handler(context) }
+            queue.sync { handler() }
         } else {
-            queue.async { handler(context) }
+            queue.async { handler() }
         }
     }
 
     private let identifier: String
+}
+
+private extension SettingsDataProvider {
+    func readValue<T>(_ type: T.Type, for key: String) -> T? {
+        UserDefaults.standard.object(forKey: effectiveKey(for: key)) as? T
+    }
+
+    func writreValue<T>(_ value: T?, for key: String) {
+        if let realValue = value {
+            UserDefaults.standard.set(realValue, forKey: effectiveKey(for: key))
+        } else {
+            UserDefaults.standard.removeObject(forKey: effectiveKey(for: key))
+        }
+    }
+
+    func effectiveKey(for key: String) -> String {
+        return "\(identifier).\(key)"
+    }
+
+    struct Keys {
+        static let sortingOrder = "sortingOrder"
+    }
 }
